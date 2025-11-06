453137c838911450b3366c310b35acf90375f36c
Nirmalkilari123 <nirmalkilari45@gmail.com>
2025-11-06 08:03:21 +0530

    a

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,19 +1,12 @@
-// import Navbar from '@/components/navbar/Navbar'
-// import React from 'react'
+import Dashboard from '@/components/Dashboard/Dashboard'
+import React from 'react'
 
-// const page = () => {
-//   return (
-//     <div>
-//        <Navbar />
-//     </div>
-//   )
-// }
-
-// export default page
-
-// app/dashboard/page.tsx
-import Dashboard from "../../components/Dashboard/Dashboard";
-
-export default function DashboardPage() {
-  return <Dashboard />;
+const page = () => {
+  return (
+    <div>
+      <Dashboard />
+    </div>
+  )
 }
+
+export default page
--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 // src/components/Dashboard.tsx
 "use client";
 
